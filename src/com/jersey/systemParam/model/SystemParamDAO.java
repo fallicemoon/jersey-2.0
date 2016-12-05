41a7d35e840c6252dd109572176abec6c02b77bb
fallicemoon <fallicemoon@gmail.com>
2016-12-05 23:23:16 +0800

    PrimaryKeyGenerator

--- src/com/jersey/systemParam/model/SystemParamDAO.java
+++ src/com/jersey/systemParam/model/SystemParamDAO.java
@@ -1,13 +1,35 @@
 package com.jersey.systemParam.model;
 
-import com.jersey.tools.AbstractDAO;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.jersey.tools.AbstractDAO;
+import com.jersey.tools.HibernateTools;
+import com.jersey.tools.JerseyEnum.PrimaryKey;
+
+@Repository
 public class SystemParamDAO extends AbstractDAO<SystemParamVO> {
+	
+	@Autowired
+	private HibernateTools hibernateTools;
 
 	public SystemParamDAO(Class<SystemParamVO> type, String pk) {
 		super(SystemParamVO.class, "name");
 	}
 	
-	
+	//TODO
+	public SystemParamVO getPrimaryKeyValueAndUpdate (PrimaryKey primaryKey) {
+		Session session = hibernateTools.getSession();
+		session.beginTransaction();
+		try {
+			SystemParamVO vo = (SystemParamVO) session.get(SystemParamVO.class, primaryKey);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		
+	} 
 
 }
--- src/com/jersey/tools/PrimaryKeyGenerator.java
+++ src/com/jersey/tools/PrimaryKeyGenerator.java
@@ -1,13 +1,21 @@
 package com.jersey.tools;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.hibernate.LockMode;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.jersey.systemParam.model.SystemParamDAO;
+import com.jersey.systemParam.model.SystemParamVO;
 import com.jersey.tools.JerseyEnum.PrimaryKey;
 
 public class PrimaryKeyGenerator {
+	
+	@Autowired
+	private SystemParamDAO systemParamDAO;
 	
 	//公開對外的pool
 	private static Map<PrimaryKey, PrimaryKeyGenerator> generatorMap = new HashMap<>();
@@ -18,20 +26,37 @@ public class PrimaryKeyGenerator {
 		return generatorMap.get(primaryKey);
 	} 
 	
+	private static DateFormat DATE_FORMAT =  new SimpleDateFormat("yyyyMMdd");
+	
 	//private Constructor
 	private PrimaryKey primaryKey;
 	private Integer currentValue;
 	private Integer maxValue;
+	
 	private PrimaryKeyGenerator (PrimaryKey primaryKey) {
 		this.primaryKey = primaryKey;
 	}
 	
 	//Generator的相關方法
-	public String getNextKey () {
-		
+	public synchronized String getNextKey () {
+		String prefix = primaryKey.getPrefix();
+		String date = DATE_FORMAT.format(Calendar.getInstance().getTime());
+		return prefix + date + getNextValue();
 	}
 	
+	private String getNextValue () {
+		if (currentValue>=maxValue) {
+			getNextValueFromDB();
+		}
+		//生成下個流水號
+		Integer prefixZeroLength = primaryKey.getLength()-currentValue.toString().length();
+		return  String.format("%0"+ prefixZeroLength +"d", currentValue.toString());
+	}
 	
+	private void getNextValueFromDB () {
+		SystemParamVO vo = systemParamDAO.getOne(primaryKey.toString());
+		vo.getValue();
+	}
 
 
 }
