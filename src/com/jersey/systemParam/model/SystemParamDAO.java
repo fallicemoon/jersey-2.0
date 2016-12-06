b8e1efc62212aee6e2d0eaf2121eae2c7b2e95f9
henry <henry@webcomm.com.tw>
2016-12-06 17:17:33 +0800

    PrimaryKeyGenerator

--- src/com/jersey/systemParam/model/SystemParamDAO.java
+++ src/com/jersey/systemParam/model/SystemParamDAO.java
@@ -1,5 +1,8 @@
 package com.jersey.systemParam.model;
 
+import java.util.Date;
+
+import org.hibernate.LockOptions;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -18,18 +21,52 @@ public class SystemParamDAO extends AbstractDAO<SystemParamVO> {
 		super(SystemParamVO.class, "name");
 	}
 	
-	//TODO
-	public SystemParamVO getPrimaryKeyValueAndUpdate (PrimaryKey primaryKey) {
+	/**
+	 * 跟DB要號並更新, 所以要lock row
+	 * Exception拋到最外面由controller處理(顯示新增失敗訊息)
+	 * @param primaryKey
+	 * @param poolSize
+	 * @return
+	 */
+	public SystemParamVO getPrimaryKeyValueAndUpdate (PrimaryKey primaryKey, Integer poolSize) {
+		Session session = hibernateTools.getSession();
+		session.beginTransaction();
+		try {
+			//TODO 要做壓力測試
+			LockOptions lockOptions = LockOptions.UPGRADE;
+			lockOptions.setTimeOut(3000);
+			SystemParamVO vo = (SystemParamVO) session.get(SystemParamVO.class, primaryKey, lockOptions);
+			vo.setValue(vo.getValue() + poolSize);
+			vo.setLastModifyTime(new Date());
+			session.update(vo);
+			session.getTransaction().commit();
+			return vo;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	/**
+	 * 流水號滿了, reset
+	 * Exception拋到最外面由controller處理(顯示新增失敗訊息)
+	 * @param primaryKey
+	 */
+	public void resetPrimaryKey (PrimaryKey primaryKey) {
 		Session session = hibernateTools.getSession();
 		session.beginTransaction();
 		try {
 			SystemParamVO vo = (SystemParamVO) session.get(SystemParamVO.class, primaryKey);
+			vo.setValue("0");
+			vo.setLastModifyTime(new Date());
+			session.update(vo);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			session.getTransaction().rollback();
 			e.printStackTrace();
+			throw e;
 		}
-		
-	} 
+	}
 
 }
--- src/com/jersey/tools/PrimaryKeyGenerator.java
+++ src/com/jersey/tools/PrimaryKeyGenerator.java
@@ -29,33 +29,62 @@ public class PrimaryKeyGenerator {
 	private static DateFormat DATE_FORMAT =  new SimpleDateFormat("yyyyMMdd");
 	
 	//private Constructor
-	private PrimaryKey primaryKey;
+	private final PrimaryKey primaryKey;
+	private final Integer dbMaxValue;
 	private Integer currentValue;
 	private Integer maxValue;
+	private static final Integer POOL_SIZE = 50;
 	
 	private PrimaryKeyGenerator (PrimaryKey primaryKey) {
 		this.primaryKey = primaryKey;
+		this.dbMaxValue = Integer.valueOf(String.format("%0"+ primaryKey.getLength() +"d", ""));
+		getNextValueFromDB();
 	}
 	
 	//Generator的相關方法
-	public synchronized String getNextKey () {
+	/**
+	 * 取得PrimaryKey
+	 * @return
+	 */
+	public synchronized String getNextPrimaryKey () {
 		String prefix = primaryKey.getPrefix();
 		String date = DATE_FORMAT.format(Calendar.getInstance().getTime());
 		return prefix + date + getNextValue();
 	}
 	
+	/**
+	 * 生成下個流水號
+	 * @return
+	 */
 	private String getNextValue () {
-		if (currentValue>=maxValue) {
+		if (currentValue > maxValue) {
 			getNextValueFromDB();
 		}
 		//生成下個流水號
 		Integer prefixZeroLength = primaryKey.getLength()-currentValue.toString().length();
-		return  String.format("%0"+ prefixZeroLength +"d", currentValue.toString());
+		String nextValue = String.format("%0"+ prefixZeroLength +"d", currentValue.toString());
+		currentValue++;
+		return nextValue;
 	}
 	
+	/**
+	 * 從DB取號並更新DB目前流水號
+	 */
 	private void getNextValueFromDB () {
-		SystemParamVO vo = systemParamDAO.getOne(primaryKey.toString());
-		vo.getValue();
+		if (currentValue+POOL_SIZE>=dbMaxValue) {
+			System.out.println("start reset DB...");
+			resetDB();
+		}
+		SystemParamVO vo = systemParamDAO.getPrimaryKeyValueAndUpdate(primaryKey, POOL_SIZE);
+		currentValue = Integer.parseInt(vo.getValue())-POOL_SIZE;
+		maxValue = Integer.parseInt(vo.getValue())-1;
+	}
+	
+	/**
+	 * 超過的話reset DB的流水號
+	 */
+	private void resetDB () {
+		systemParamDAO.resetPrimaryKey(primaryKey);
 	}
 
 
